c3b11b945dfea7b5aba88cfb42477ffaf9bf428c
skyriv213 <80723571+skyriv213@users.noreply.github.com>
2024-02-23 21:03:19 +0900

    #22 <feature> <(be - cors)> : cors 설정을 위한 security config 작성 (#23)
    Co-authored-by: skyriv213 <>
    close #23

--- be/src/main/java/com/example/be/common/config/SecurityConfig.java
+++ be/src/main/java/com/example/be/common/config/SecurityConfig.java
@@ -1,23 +1,50 @@
 package com.example.be.common.config;
 
 
-//@EnableWebSecurity
-//@Configuration
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+@EnableWebSecurity
+@Configuration
 public class SecurityConfig   {
 
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.requiresChannel()
-//            .anyRequest()
-//            .requiresSecure();
-////    }
-//
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-//        httpSecurity.authorizeHttpRequests((auth) ->
-//            auth
-//                .requestMatchers("/store/**").permitAll()
-//                .anyRequest().authenticated());
-//        return httpSecurity.build();
-//    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity.cors((cors) -> cors.configurationSource(corsConfigurationSource()));
+
+        httpSecurity.csrf((csrf) -> csrf.disable());
+
+        httpSecurity.headers((headers) ->
+            headers.xssProtection(Customizer.withDefaults())
+                .contentSecurityPolicy(Customizer.withDefaults()));
+
+
+        httpSecurity.authorizeHttpRequests((auth) ->
+            auth
+                .requestMatchers("/store/**").permitAll()
+                .anyRequest().authenticated());
+
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("*");
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 }
--- be/src/main/java/com/example/be/common/config/WebConfig.java
+++ be/src/main/java/com/example/be/common/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.example.be.common.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,9 +8,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer{
 
+//    @Bean
 //    @Override
 //    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//            .allowedMethods()
+//        registry.addMapping("**/**")
+//            .allowedMethods("GET", "POST", "PATCH", "DELETE", "PUT", "OPTIONS") // OPTIONS 추가
+//            .allowedHeaders("*") // 필요한 경우 모든 헤더를 허용
+//            .allowCredentials(true) // 필요한 경우 쿠키/인증 정보 포함
+//            .allowCredentials(true) // 쿠키 인증 요청 허용
+////            .exposedHeaders("Authorization", "Refresh")
+//            .maxAge(3600); // preflight 결과를 1시간 동안 캐
 //    }
 }
