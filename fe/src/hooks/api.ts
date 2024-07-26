d502a1156d0c73a78898466d2de857cd4669f2f1
benidene <111226162+benidene@users.noreply.github.com>
2024-07-27 01:03:44 +0900

    Fe report cors (#81)
    * #79 <fix> : http-proxy-middleware 라이브러리 설치
    
    * #79 <feat> : proxy 코드 추가
    
    * #59 <fix> : report api 및 url 수정
    
    * #79 <fix> : postApi url 수정

--- fe/src/hooks/api.ts
+++ fe/src/hooks/api.ts
@@ -10,14 +10,14 @@ export async function getApi (url:string) {
 
 export async function postApi (url:string, newData:string | object) {
     try {
-      const res = await fetch(`http://localhost:8080${url}`, {
+      const res = await fetch(`/api${url}`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify(newData)
       });
-
+      
       return await res.json()
     }catch (error) {
     console.error(error);
